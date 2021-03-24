a0373266f6b0a73fc2441a9768a98be04322f016
dohwan.ahn <dohwan.an@gmail.com>
2021-03-24 23:35:24 +0900

    Study 210324

--- HelloWorld/src/main/kotlin/com/hwanyu365/classes/Classes.kt
+++ HelloWorld/src/main/kotlin/com/hwanyu365/classes/Classes.kt
@@ -2,20 +2,69 @@ package com.hwanyu365.classes
 
 class Classes {
     fun run() {
-        InitOrderDemo("InitOrder")
+        val name = "hwanyu365"
+
+        // Kotlin does not have a new keyword.
+        InitOrderDemo(37).introduce()
+//        InitOrderDemo("hwanyu365", 37).introduce()
+
+        // constructor
+        HideDefaultConstructor(name)
+
+        // open, abstract
+        Triangle().introduce()
+        Car().move()
     }
 }
 
-class InitOrderDemo(name: String) {
+class InitOrderDemo private constructor(private val name: String, var age: Int) { // trailing comma
     val firstProperty = "First property: $name".also(::println)
 
     init {
-        println("First initializer block that prints ${name}")
+        println("First initializer block that prints $name")
     }
 
     val secondProperty = "Second property: ${name.length}".also(::println)
 
     init {
         println("Second initializer block that prints ${name.length}")
+    }
+
+    fun introduce() {
+        println("Name: $name, Age: $age")
+    }
+
+    // If the class has a primary constructor, each secondary constructor needs to delegate to the primary constructor,
+    // either directly or indirectly through another secondary constructor(s).
+    constructor(age: Int) : this("hwanyu365", age) {
+        println("initialized through secondary constructor")
+    }
+}
+
+class HideDefaultConstructor private constructor() {
+    constructor(name: String) : this() {
+        println("HideDefaultConstructor.name is $name")
+    }
+}
+
+open class Shape constructor(private val name: String = "Shape", private val vertex: Int = 0) {
+    fun introduce() {
+        println("name=$name, vertex=$vertex")
+    }
+}
+
+class Triangle constructor(name: String = "Triangle", vertex: Int = 3) : Shape(name, vertex)
+
+abstract class Vehicle constructor(private val name: String = "Vehicle", private val cc: Int = 0) {
+    fun introduce() {
+        println("name=$name, cc=$cc")
+    }
+
+    abstract fun move()
+}
+
+class Car constructor(name: String = "Car", cc: Int = 1600) : Vehicle(name, cc) {
+    override fun move() {
+        println("Brrrrrr")
     }
 }
